2789689534c15ce7f4ebd377136b2e0e93dc14f0
студент2 <2@DESKTOP-QEFI0G7>
2022-11-26 11:23:24 +0300

    Добавьте файлы проекта.

---
+++ Restoran/MenuRestoran.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restoran
+{
+    internal class MenuRestoran
+    {
+        public void Comparison(List<string> menu,string order)
+        {
+            List<string> list = new List<string>();
+
+            foreach(var str2 in menu)
+            {
+                if (order.Contains(str2))
+                {
+                    list.Add(str2 + ' ') ;
+                }
+            }
+            foreach(var str3 in list)
+            {
+                order.ToUpper();
+                Console.Write(str3);
+            }
+        }
+    }
+}
---
+++ Restoran/Program.cs
@@ -0,0 +1,33 @@
+﻿using Restoran;
+
+MenuRestoran menuRestoran = new MenuRestoran();
+
+void Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine("Меню ресторана");
+    Console.WriteLine();
+    Console.WriteLine("1. Burger");
+    Console.WriteLine("2. Fries");
+    Console.WriteLine("3. Chiken");
+    Console.WriteLine("4. Pizza");
+    Console.WriteLine("5. Sandwich");
+    Console.WriteLine("6. Onionrings");
+    Console.WriteLine("7. Milkshake");
+    Console.WriteLine("8. Coke");
+}
+void funcMenu()
+{
+    List<string> list = new List<string>()
+    {
+        "burger","fries","chiken","pizza","sandwich","onionrings","milkshake","coke",
+    };
+    string enter = Console.ReadLine();
+   menuRestoran.Comparison(list,enter);
+    enter.ToUpper();
+}
+while (true)
+{
+Menu();
+funcMenu();
+}
